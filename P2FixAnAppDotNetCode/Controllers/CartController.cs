4ddddc89e937565ba0468d857f4b96913c884c41
Olivier Dupuy <olivier.dupuy74@hotmail.fr>
2023-11-12 00:51:06 +0100

    Ajoutez des fichiers projet.

---
+++ P2FixAnAppDotNetCode/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using P2FixAnAppDotNetCode.Models;
+using P2FixAnAppDotNetCode.Models.Services;
+
+namespace P2FixAnAppDotNetCode.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICart _cart;
+        private readonly IProductService _productService;
+
+        public CartController(ICart pCart, IProductService productService)
+        {
+            _cart = pCart;
+            _productService = productService;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_cart as Cart);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult AddToCart(int id)
+        {
+            Product product = _productService.GetProductById(id);
+
+            if (product != null)
+            {
+                _cart.AddItem(product, 1);
+                _productService.UpdateProductQuantities(_cart as Cart);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Product");
+            }
+        }
+
+        public RedirectToActionResult RemoveFromCart(int id)
+        {
+            Product product = _productService.GetAllProducts()
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product != null)
+            {
+                _cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ P2FixAnAppDotNetCode/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2FixAnAppDotNetCode.Models;
+using P2FixAnAppDotNetCode.Models.Services;
+using System.Collections.Generic;
+
+namespace P2FixAnAppDotNetCode.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ILanguageService _languageService;
+        private readonly ICart _cart;
+
+        public ProductController(IProductService productService, ILanguageService languageService, ICart pCart)
+        {
+            _cart = pCart;
+            _productService = productService;
+            _languageService = languageService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _productService.GetAllProducts();
+            _productService.UpdateProductQuantities(_cart as Cart);
+            return View(products);
+        }
+    }
+}
